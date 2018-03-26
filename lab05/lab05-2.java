2bafa6cc4e469dfc5c6f7b5255aea27dea56d227
JattMones <jonesm2@allegheny.edu>
2018-03-26 11:23:12 -0400

    Add files via upload

---
+++ lab05/lab05-2.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class lab05-2{
+  public static int confirmValid(int s ArrayList k) {  //method that confirms the spot is a valid place, and changes the spot if necessary
+      if(k.contains(s)){
+        return s;
+      }
+      else{
+        //add 1 to s, check if s+1 is in our arraylist recursively
+      }
+  public static String printQueens(){
+    //use variables r for row, and s for spot selection to print our solution row
+    //by row.
+    
+  }
+  }
+}

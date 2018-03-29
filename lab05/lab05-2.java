3a89ac9581d6d424eb8a60b2a190645aa8f028eb
JattMones <jonesm2@allegheny.edu>
2018-03-29 12:12:02 -0400

    Update lab05-2.java

--- lab05/lab05-2.java
+++ lab05/lab05-2.java
@@ -1,17 +1,12 @@
 import java.util.ArrayList;
 
-public class lab05-2{
-  public static int confirmValid(int s ArrayList k) {  //method that confirms the spot is a valid place, and changes the spot if necessary
+public class lab5{
+  public static boolean confirmValidPlacement(int s, ArrayList k){
       if(k.contains(s)){
-        return s;
+        return true;
       }
       else{
-        //add 1 to s, check if s+1 is in our arraylist recursively
+        return false;
       }
-  public static String printQueens(){
-    //use variables r for row, and s for spot selection to print our solution row
-    //by row.
-    
-  }
   }
 }
